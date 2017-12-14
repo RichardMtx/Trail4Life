3619ea9845705b09a20453b44ce4176a809a3775
RichardMtx <RichardMtx@github.com>
2017-12-14 19:06:47 +0100

    Ajout de LaraAdmin

--- config/app.php
+++ config/app.php
@@ -147,6 +147,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Dwij\Laraadmin\LAProvider::class,
 
         /*
          * Application Service Providers...
